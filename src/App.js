bd05664e53fc4315c55eebc56db7c49e57260eb0
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-12 04:26:15 +0300

    Update Chakra UI imports and add Framer Motion animation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ChakraProvider, Box, Link, Icon, Text, Flex, VStack } from "@chakra-ui/react";
+import { ChakraProvider, Container, VStack, Flex, Text, Link, Icon } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 import theme from "./theme";
 import Header from "./components/Header";
 import EducationalContent from "./components/EducationalContent";
@@ -9,25 +10,28 @@ import MindfulnessSection from "./components/MindfulnessSection";
 import ResourcesSection from "./components/ResourcesSection";
 import Footer from "./components/Footer";
 
+const MotionContainer = motion(Container);
+
 const App = () => {
+  const containerVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } }
+  };
+
   return (
     <ChakraProvider theme={theme}>
       <VStack minHeight="100vh" justify="space-between" width="100%">
-        <Box>
+        <MotionContainer maxW="container.xxl" variants={containerVariants} initial="hidden" animate="visible">
           <Header />
           <EducationalContent />
           <Quiz />
           <MindfulnessSection />
           <ResourcesSection />
-        </Box>
-        <Box>
+        </MotionContainer>
+
+        <Container maxW="container.xxl">
           <Footer />
-          <Flex
-            textAlign="center"
-            p={4}
-            alignItems="center"
-            justifyContent="center"
-          >
+          <Flex textAlign="center" paddingTop="0.5rem" alignItems="center" justifyContent="center">
             <Text fontSize="xs">
               Ⓒ جميع حقوق النشر للنطاقات تحت hadi.zone محفوظة لـ هادي المرزوق
             </Text>
@@ -35,7 +39,7 @@ const App = () => {
               <Icon as={FaGithub} mx={1} />
             </Link>
           </Flex>
-        </Box>
+        </Container>
       </VStack>
     </ChakraProvider>
   );
