0efbdaf642d542aa19b5573290b90c8074372872
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-12 03:39:49 +0300

    Add margin to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Box, Heading } from '@chakra-ui/react';
 
 const Header = () => {
   return (
-    <Box as="header" bg="primary.100" p={4}>
+    <Box as="header" bg="primary.100" p={4} margin="1rem">
       <Heading as="h1" size="xl">كيف نخفف من ادمان مواقع التواصل الاجتماعي</Heading>
     </Box>
   );
