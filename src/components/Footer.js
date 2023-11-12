ffb3c03412c16e0332fa374383aecf5a08f104c0
HadiAlMarzooq <ahcraft1@gmail.com>
2023-11-12 04:27:32 +0300

    Refactor Footer component styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box as="footer" bg="gray.200" p={1} textAlign="center" color="primary.900">
+    <Box
+      as="footer"
+      bg="gray.200"
+      p={1}
+      textAlign="center"
+      color="primary.900"
+      borderRadius="md"
+    >
+      {" "}
       <Text fontSize="sm">أعضاء المشروع:</Text>
       <Text fontSize="sm">1- هادي المرزوق</Text>
       <Text fontSize="sm">2- بدر الوقيصي</Text>
